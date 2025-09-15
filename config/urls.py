d8e5de374ae8bdec6bf876c8a41bcf38254fa179
saastern <saastern@yahoo.com>
2025-09-15 02:06:03 -0400

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -28,7 +28,7 @@ urlpatterns = [
      path('assessments/', include('apps.assessments.urls')),
     path('api/teachers/', include('apps.teachers.urls')),
     path('api/', include('apps.assessments.urls')),
-    path('api/attendance/', include('apps.attendance.urls')),
+    path('', include('apps.attendance.urls')),
    
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
